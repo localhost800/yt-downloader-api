644defb1a31aa44f0e3b1be126afe50ccdb81a38
localhost800 <pepecastillo0101@gmail.com>
2023-02-05 00:32:38 -1000

    Testing requests

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import express from 'express';
+import ytdl from 'ytdl-core';
 const app = express();
+app.use(express.urlencoded({extended: true}));
 app.get("/", (req, res)=>{
 	res.send("Helloooo from Linux!");
 });
+app.post("/download", (req, res)=>{
+	const url = req.body.url;
+	const date = Date.now();
+	res.header("Content-Disposition", `attachment; filename="linkit_aud_${date}.mp3"`);
+	return ytdl(url, {filter: 'audioonly', format: 'mp3'}).pipe(res);
+}
 app.listen(3000, ()=>{
 	console.log("Server listening on port", 3000);
 });
