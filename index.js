81e4f410e1ae13bbaf0df91f799e46cb55ec7c08
localhost800 <pepecastillo0101@gmail.com>
2023-02-05 01:32:23 -1000

    Testing requests

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import ytdl from 'ytdl-core';
+import cors from 'cors';
 const app = express();
 app.use(express.urlencoded({extended: true}));
+app.use(cors());
 app.get("/", (req, res)=>{
 	res.send("Helloooo from Linux!");
 });
